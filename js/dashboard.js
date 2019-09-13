bc911013c3cff665bc1d1613ec231b1f4ed64f6f
Abayomi igwubor <igwubor@outlook.com>
2019-09-13 15:42:24 +0100

    madeDashBoardBetter

--- js/dashboard.js
+++ js/dashboard.js
@@ -101,3 +101,15 @@ signUpform.addEventListener('submit', (e) => {
 		incomingData.innerHTML = clients_info_outputted;
 	});
 });
+
+$(document).ready(function() {
+	$('.first-button').on('click', function() {
+		$('.animated-icon1').toggleClass('open');
+	});
+	$('.second-button').on('click', function() {
+		$('.animated-icon2').toggleClass('open');
+	});
+	$('.third-button').on('click', function() {
+		$('.animated-icon3').toggleClass('open');
+	});
+});
